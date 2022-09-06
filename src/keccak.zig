47719cfb67cc05673f72463928c1022e079d170c
cod1r <jasonho353@gmail.com>
2022-09-06 15:34:08 -0500

    using testing.allocator which i'm assuming the compiler insert the deinit() to detect leaks

--- src/keccak.zig
+++ src/keccak.zig
@@ -230,9 +230,7 @@ test "REV" {
 }
 
 test "sha3 224" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{.safety = true}){};
-    defer _ = gpa.deinit();
-    var alloc = gpa.allocator();
+    var alloc = std.testing.allocator;
     const ans: []const u8 = "6b4e03423667dbb73b6e15454f0eb1abd4597f9a1b078e3f5b5a6bc7";
     const in: []const u8 = "";
     const out: []u8 = try keccak(alloc, b - 2 * 224, in, 224 / 8, 0x06);
@@ -260,9 +258,7 @@ test "sha3 224" {
 }
 
 test "sha3 256" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{.safety = true}){};
-    defer _ = gpa.deinit();
-    var alloc = gpa.allocator();
+    var alloc = std.testing.allocator;
     const ans: []const u8 = "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a";
     const in: []const u8 = "";
     const out: []u8 = try keccak(alloc, b - 2 * 256, in, 256 / 8, 0x06);
@@ -290,9 +286,7 @@ test "sha3 256" {
 }
 
 test "sha3 384" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{.safety = true}){};
-    defer _ = gpa.deinit();
-    var alloc = gpa.allocator();
+    var alloc = std.testing.allocator;
     const ans: []const u8 = "0c63a75b845e4f7d01107d852e4c2485c51a50aaaa94fc61995e71bbee983a2ac3713831264adb47fb6bd1e058d5f004";
     const in: []const u8 = "";
     const out: []u8 = try keccak(alloc, b - 2 * 384, in, 384 / 8, 0x06);
@@ -320,9 +314,7 @@ test "sha3 384" {
 }
 
 test "sha3 512" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{.safety = true}){};
-    defer _ = gpa.deinit();
-    var alloc = gpa.allocator();
+    var alloc = std.testing.allocator;
     const ans: []const u8 = "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26";
     const in: []const u8 = "";
     const out: []u8 = try keccak(alloc, b - 2 * 512, in, 512 / 8, 0x06);
@@ -350,9 +342,7 @@ test "sha3 512" {
 }
 
 test "shake 128" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{.safety = true}){};
-    defer _ = gpa.deinit();
-    var alloc = gpa.allocator();
+    var alloc = std.testing.allocator;
     const ans: []const u8 = "7f9c2ba4e88f827d616045507605853ed73b8093f6efbc88eb1a6eacfa66ef26";
     const in: []const u8 = "";
     const out: []u8 = try keccak(alloc, b - 2 * 128, in, 256 / 8, 0x1F);
@@ -380,9 +370,7 @@ test "shake 128" {
 }
 
 test "shake 256" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{.safety = true}){};
-    defer _ = gpa.deinit();
-    var alloc = gpa.allocator();
+    var alloc = std.testing.allocator;
     const ans: []const u8 = "46b9dd2b0ba88d13233b3feb743eeb243fcd52ea62b81b82b50c27646ed5762fd75dc4ddd8c0f200cb05019d67b592f6fc821c49479ab48640292eacb3b7c4be";
     const in: []const u8 = "";
     const out: []u8 = try keccak(alloc, b - 2 * 256, in, 512 / 8, 0x1F);
